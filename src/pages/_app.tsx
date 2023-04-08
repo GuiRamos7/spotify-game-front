c9ef842f63888500a4612b2c56465de08342338a
Guilherme Ramos <guioliveiraramos10@gmail.com>
2023-04-08 01:19:06 -0300

    feat: set up chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Inter } from 'next/font/google';
+
+import type { AppProps } from 'next/app';
+import { theme } from '@/styles/theme';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </main>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const [songs, setSongs] = useState([]);
@@ -51,14 +48,3 @@ export default function Home() {
     </>
   );
 }
-
-// <iframe
-//   style='border-radius:12px'
-//   src='https://open.spotify.com/embed/playlist/37i9dQZF1DWXRqgorJj26U?utm_source=generator&theme=0'
-//   width='100%'
-//   height='352'
-//   frameBorder='0'
-//   allowfullscreen=''
-//   allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
-//   loading='lazy'
-// ></iframe>;
